18b919963f270dfe4721eb4f6b738d2aab89212c
Hanifah Marta Ardilah <105196353+Hanifahmarta@users.noreply.github.com>
2023-05-25 23:28:44 +0700

    Update minio.js

--- src/config/minio.js
+++ src/config/minio.js
@@ -1,59 +1,31 @@
-var Minio = require ('minio')
+const Minio = require('minio');
+require('dotenv').config();
 
 
-// credensial minio
-var minioClient = new Minio.Client({
-    endPoint: '172.20.0.84',
-    port: 9000,
-    useSSL: false,
-    accessKey: 'xjUQ3DwRN4LepbaH',
-    secretKey: 'NQTArldunpvAWZJhZOrarAdvjjpjNXKc'
+const minioClient = new Minio.Client({
+  endPoint: process.env.MINIO_HOST,   
+  port: 9000,
+  useSSL: false,
+  accessKey: process.env.MINIO_ACCESS_KEY,
+  secretKey: process.env.MINIO_SECRET_KEY,
 });
+module.exports = minioClient;
 
+// Check connection to minio
+minioClient.bucketExists('testing', function(err, exists) {
+    if (err) {
+        return console.log(err)
+    }
+    if (exists) {
+        console.log('Bucket exists.')
+    } else {
+        console.log('Bucket does not exist.')
+    }
+})
 
-// Melihat list bucket bisa
-// minioClient.listBuckets(function(err, buckets) {
-//     if (err) return console.log(err)
-//     console.log('buckets :', buckets)
-//   });
-
-
-// Create bucket tobe public
-// minioClient.setBucketPolicy('testing', 'public-read', function(err) {
-//     if (err) {
-//       return console.log(err)
-//     }
-//     console.log('Bucket mybucket is now public')
-//   })
-
-
-// Get link URL public from file in bucket (done)
-// Get Link URL public from file in bucket dgn waktu kedaluwarsa dari URL yang dikembalikan, dalam detik.
-//   minioClient.presignedUrl('GET', 'testing', 'kitten.png', 60 * 60 * 24, function(err, url) {
-// // 60*60*24 = 1 hari => Itu akan mencetak URL public dari file 'myfile.jpg' di bucket 'mybucket' yang akan kadaluwarsa dalam 24 jam.
-//     if (err) {
-//       return console.log(err)
-//     }
-//     console.log(url)
-//   })
-
-// Upload file to bucket (done)
-//   minioClient.fPutObject('testing2', 'kitten.png', 'E:/Capstone Server/kitten.png', function(err, etag) {
-//     if (err) {
-//       return console.log(err)
-//     }
-//     console.log('File uploaded successfully. eTag: ' + etag)
-//   })
-
-minioClient.fPutObject('testing2', 'american-sign-language_1.tar.gz', 'E:/Capstone Server/american-sign-language_1.tar.gz', function(err, etag) {
+ minioClient.presignedGetObject('testing', 'kitten.png', 24 * 60 * 60, function(err, presignedUrl) {
     if (err) {
       return console.log(err)
     }
-    console.log('File uploaded successfully. eTag: ' + etag)
+    console.log('The public URL is', presignedUrl)
   })
-
-
-
-
-
-
