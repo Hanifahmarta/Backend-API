81d599aca350a89369e91ba4ca19caeaaf0e3800
Hanifah Marta Ardilah <105196353+Hanifahmarta@users.noreply.github.com>
2023-05-25 23:59:48 +0700

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,17 +1,27 @@
 const { Router } = require('express');
-const controller = require('../controller/user');
+const user = require('../controller/user');
+const usercontri =  require('../controller/UserContributor');
+const article = require('../controller/article-controller');
+const video = require('../controller/Video');
 
 const router = Router();
 
-router.post('/signup', controller.createuser);
-router.post('/signin', controller.loginuser);
+router.post('/signup/contributor', usercontri.usercontributor);
+router.post('/signin/contributor', usercontri.loginusercontributor);
+router.get('/getall/contributor', usercontri.getusercontributor);
+router.post ('/uplod/profile', usercontri.uploadprofile);
+router.get('/signout', usercontri.signoutcontributor);
+router.delete('/delete/:id', usercontri.deleteusercontributor);
 
-router.get('/getall', controller.getuser);
-// router.get('/:id', controller.getuserbyid);
-router.get('/get', controller.getusercontributor);
+router.post('/upload/video', video.uploadVideo);
+router.get('/get/video', video.getVideo);
+router.get('/result', video.result);
+router.get('/ranking', video.ranking);
 
-router.put('/edit/:id', controller.edituser);
-router.put('/upload/:id', controller.updatevideo);
+router.get('/get/article', article.getArticle);
+router.get('/article/:id', article.getArticleById);
+router.post('/addfav/:id', article.addArticle);
+router.post('/addfav', article.addArticle);
+router.get('/getfav', article.getFavorite);
 
-router.post('/upload/video', controller.uploadvideo);
 module.exports = router;
