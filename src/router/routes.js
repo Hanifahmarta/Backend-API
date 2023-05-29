c966cb9ead28d4461cc7c1614bd613206618c1f0
Hanifah Marta Ardilah <105196353+Hanifahmarta@users.noreply.github.com>
2023-05-29 13:52:16 +0700

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,14 +4,12 @@ const video = require('../controller/Video');
 const router = Router();
 const uploads = require('../controller/Upload');
 const multer = require('multer');
-
 const storage = multer.diskStorage({
     destination: './assets/images',
     filename: function (req, file, cb) {
         cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
     }
 })
-
 const upload = multer({
     storage: storage,
  });
