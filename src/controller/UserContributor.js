863387a0975c797b9659b0c79e337237e9399e04
Hanifah Marta Ardilah <105196353+Hanifahmarta@users.noreply.github.com>
2023-05-29 13:27:26 +0700

    Update UserContributor.js

--- src/controller/UserContributor.js
+++ src/controller/UserContributor.js
@@ -5,13 +5,13 @@ const crypto = require('crypto');
 
 //Regiter user contributor token
 const usercontributor = (req, res) => {
-    const data = { 
+    const data = {
         username: req.body.username,
         email: req.body.email,
         password: req.body.password,
-        nomorhp: req.body.nomorhp,
+        nohp: req.body.nohp
         photo: req.body.photo,
-        token: crypto.randomBytes(20).toString('hex')
+        token: crypto.randomBytes(20).toString('hex'),
     }
     //check if email already exists
     pool.query('SELECT * FROM usercontributor WHERE email = $1', [data.email], (error, results) => {
@@ -21,9 +21,9 @@ const usercontributor = (req, res) => {
             // Hash the password
             bcrypt.hash(data.password, 10, function(err, hash) {
             //insert user
-            pool.query('INSERT INTO usercontributor (username, email, password, nomorhp, photo, token) VALUES ($1, $2, $3, $4, $5, $6)', [data.username, data.email, hash, data.nomorhp, data.photo, data.token], (error, results) => {
+            pool.query('INSERT INTO usercontributor (username, email, password, nohp, photo, token) VALUES ($1, $2, $3, $4, $5, $6)', [data.username, data.email, hash, data.nohp, data.photo, data.token], (error, results) => {
                 if (error) throw error;
-                res.status(201).json({message: 'User created successfully', data: req.body})
+                res.status(201).json({message: 'User created successfully', data: req.data})
             });
         });
         }
@@ -46,8 +46,8 @@ const loginusercontributor = (req, res) => {
                         id: results.rows[0].id,
                         username: results.rows[0].username,
                         email: results.rows[0].email,
-                        nomorhp: results.rows[0].nomorhp,
-                        photo: results.rows[0].photo
+                        nohp: results.rows[0].nohp,
+                        photo: results.rows[0].photo,
                     };
                     req.session.isLoggedIn = true;
                     res.status(200).json({message: 'Login successful', data: req.body})
@@ -61,15 +61,24 @@ const loginusercontributor = (req, res) => {
     });
 }
 
+//Check if the user is logged in with session
+const checkuser = (req, res) => {
+    if (req.session.user) {
+        res.status(200).json({message: 'User logged in', data: req.session.user})
+    } else {
+        res.status(400).json({message: 'User not logged in'})
+    }
+}
+
 
 // Edit user contributor
 const editusercontributor = (req, res) => {
     const id = parentInt(req.params.id)
-    const { username, email, password, nomorhp, photo } = req.body
+    const { username, email, password, nohp, photo } = req.body
 
     bcrypt.hash (password, 10, function(err, hash) {
         //update user
-        pool.query('UPDATE usercontributor SET username = $1, email = $2, password = $3, nomorhp = $4, photo = $5 WHERE id = $6', [username, email, hash, nomorhp, photo, id], (error, results) => {
+        pool.query('UPDATE usercontributor SET username = $1, email = $2, password = $3, nohp = $4, photo = $5  WHERE id = $6', [username, email, hash, photo, nohp, id], (error, results) => {
             if (error) throw error;
             res.status(200).json({message: 'User updated successfully', data: req.body})
         });
@@ -81,16 +90,6 @@ const getusercontributor = (req, res) => {
     pool.query('SELECT * FROM usercontributor', (error, results) => {
         if (error) throw error;
         res.status(200).json(results.rows)
-    });
-}
-
-// Upload photo to minio bucket
-const uploadprofile = (req, res) => {
-    const { photo } = req.body
-
-    minioClient.putObject('testing', photo, (error, results) => {
-        if (error) throw error;
-        res.status(201).json({message: 'Photo uploaded successfully', data: req.body})
     });
 }
 
@@ -107,14 +106,20 @@ const deleteusercontributor = (req, res) => {
     });
 }
 
+const checksignout = (req, res) => {
+    if (req.session.user) {
+        res.send('User logged in')
+    } else {
+        res.send('User already logout')
+    }
+};
 
 module.exports = {
     usercontributor,
     loginusercontributor,
     editusercontributor,
-    uploadprofile,
     getusercontributor,
     signoutcontributor,
-    deleteusercontributor
-    
-}   
+    deleteusercontributor,
+    checksignout
+}
