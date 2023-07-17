3d1c71adac469f009f8305045296bd0bbae49074
Hanifah Marta Ardilah <105196353+Hanifahmarta@users.noreply.github.com>
2023-07-17 14:20:18 +0700

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
 const usercontri =  require('../controller/UserContributor');
 const video = require('../controller/Video');
-const router = Router();
+const article = require('../controller/article-controller');
 const uploads = require('../controller/Upload');
-const multer = require('multer');
+ const multer = require('multer');
 const storage = multer.diskStorage({
     destination: './assets/images',
     filename: function (req, file, cb) {
@@ -12,29 +12,31 @@ const storage = multer.diskStorage({
 })
 const upload = multer({
     storage: storage,
- });
+});
+const router = Router();
 
 router.post('/signup/contributor', usercontri.usercontributor);
 router.post('/signin/contributor', usercontri.loginusercontributor);
+router.post('/get/auth', usercontri.userauth);
 router.get('/checkuser', usercontri.checkuser);
 router.get('/getall/contributor', usercontri.getusercontributor);
 router.put('/edit/contributor/:id', usercontri.editusercontributor);
 router.get('/signout', usercontri.signoutcontributor);
 router.get('/checkout', usercontri.checksignout);
 router.delete('/delete/:id', usercontri.deleteusercontributor);
-router.post('/upload/video', video.uploadVideo);
+router.post('/upload/profile', upload.single('photo'), uploads);
+router.post('/upload/videos', upload.single('video'), uploads);
+router.post('/upload/video/name', video.uploadVideo);
 router.get('/get/video', video.getVideo);
 router.get('/result', video.result);
 router.get('/ranking', video.ranking);
 router.get('/history/user', video.history);
 router.delete('/delete/upload/:id', video.deleteHistory);
-router.post('/upload/image', upload.single('photo'), uploads);
-router.post('/upload/videos', upload.single('video'), uploads);
 router.get('/get/article', article.getArticle);
 router.get('/article/:id', article.getArticleById);
 router.post('/addfav/:id', article.addArticle);
-router.get('/getfav', article.getFavorite);
-router.delete('/delete/article/:id', article.removeFavorite);
+router.put('/update/photo/:id', usercontri.updatephoto );
+router.put('/update/video/:id', usercontri.updatevideo );
 
 
 module.exports = router;
